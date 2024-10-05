cb01f71633a58614834a7117d37e107b46a57d23
Shivansh Raghav <shivansh901@gmail.com>
2024-10-05 19:21:00 +0530

    configured auth & shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { auth } from '@/auth';
+import SignIn from '@/components/SignIn';
+import { SignOut } from '@/components/SignOut';
+
 export default async function Home() {
+  const session = await auth();
+
   return (
     <main>
-      <h1>Welcome</h1>
+      <h1>Welcome {session?.user?.name}</h1>
+      {session?.user ? <SignOut /> : <SignIn />}
     </main>
   );
 }
